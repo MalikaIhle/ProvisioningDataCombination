db6ba919e560072df8089f2182a48bc8a07fe365
Malika Ihle <mihle@mpio.orn.mpg.de>
2016-06-17 10:17:10 +0200

    add install packages for new computer

--- AlternationSynchronyDataAnalyses.R
+++ AlternationSynchronyDataAnalyses.R
@@ -6,7 +6,6 @@
 #	 commit: analyses on synchrony 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
 {### remarks
 # LastSeenAlive information needs to be updated manually when DB updated
 # MY_tblBrood$Nb3 is the number of post fledgling
@@ -21,6 +20,25 @@
 rm(list = ls(all = TRUE))
 
 {### packages
+
+# install.packages("beepr")
+# install.packages("RODBC")
+# install.packages("MASS")
+# install.packages("dplyr")
+# install.packages("ggplot2")
+# install.packages("boot")
+# install.packages("lme4")
+# install.packages("arm")
+# install.packages("RLRsim")
+# install.packages("MCMCglmm")
+# install.packages("R2admb")
+# install.packages("glmmADMB", 
+    # repos=c("http://glmmadmb.r-forge.r-project.org/repos",
+            # getOption("repos")),
+    # type="source")
+
+
+
 library(beepr) # so that markdown beeps when error generated
 library(RODBC)
 library(MASS) # for boxcox
